a5588f8dc9923883a842b84f6fcd6dd6eb717189
junecheolpark <skswnscjf2@naver.com>
2025-06-29 23:57:58 +0900

    save

--- web/src/main/webapp/resources/js/layout.js
+++ web/src/main/webapp/resources/js/layout.js
@@ -1,4 +1,17 @@
-﻿$(function() {
-	
+﻿$(function () {
+    $('#btnMenu').click(function () {
+        const objImg = $('#btnMenu').children('img')
+            , objList = $('#menuAll');
+
+        if (objList.css('display') == 'none') {
+            objImg.attr('src', '/resources/images/btn/btn_menu_close.png');
+            objList.show();
+        } else {
+            objImg.attr('src', '/resources/images/btn/btn_menu_open.png');
+            objList.hide();
+        }
+
+        return false;
+    });
 });
 
