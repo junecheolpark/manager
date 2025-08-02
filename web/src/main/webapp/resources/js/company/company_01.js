0455a8bcecdfe812aaae32df7526f64cc441b265
junecheolpark <skswnscjf2@naver.com>
2025-08-02 23:59:07 +0900

    company

--- web/src/main/webapp/resources/js/company/company_01.js
+++ web/src/main/webapp/resources/js/company/company_01.js
@@ -1,2 +1,8 @@
+let _pageSize = 11, _curPage = 1;
 
-
+$(function() {
+	fnCodeSelList([1, 38, '', '구분', 0, true, 0], $('#selSchComTp'));
+	fnCodeSelList([1, 39, '', '상태', 0, true, 0], $('#selSchComSts'));
+	fnCodeSelList([1, 38, '', '선택', 0, true, 0], $('#selComTp'));
+	fnCodeSelList([1, 39, '', '선택', 0, true, 0], $('#selComSts'));
+});
